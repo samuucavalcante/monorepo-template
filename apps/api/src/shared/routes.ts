e8349a0d7fe4ad7016f2c12bec47e281701b6a9f
Samuel Cavalcante <samuel-icm-dantas@hotmail.com>
2025-07-27 20:27:34 -0300

    refactor: remove unused type import from routes

--- apps/api/src/shared/routes.ts
+++ apps/api/src/shared/routes.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import authRouter from "@modules/auth/auth.routes";
 import userRouter from "@modules/user/user.routes";
-import type { User } from "shared";
 
 const appRouter = Router();
 
